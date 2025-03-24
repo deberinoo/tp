60e149cb5d042b4516e762ad7f17ff2f5c3fbc6e
tadacheng <tang.dancheng@gmail.com>
2025-03-24 15:04:43 +0800

    Update note prefix to fit EduEase prefix format

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,9 +11,9 @@ public class CliSyntax {
     public static final Prefix PREFIX_PARENTPHONE = new Prefix("pp/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_APPEND = new Prefix("-a");
-    public static final Prefix PREFIX_OVERWRITE = new Prefix("-o");
-    public static final Prefix PREFIX_CLEAR = new Prefix("-c");
+    public static final Prefix PREFIX_APPEND = new Prefix("a/");
+    public static final Prefix PREFIX_OVERWRITE = new Prefix("o/");
+    public static final Prefix PREFIX_CLEAR = new Prefix("c/");
     public static final Prefix PREFIX_SUBJECT = new Prefix("s/");
     public static final Prefix PREFIX_DATE = new Prefix("d/");
     public static final Prefix PREFIX_TIME = new Prefix("t/");
