5a4e85fc89fdd77fa1c3768aa6fe6b007a8664f2
tadacheng <tang.dancheng@gmail.com>
2025-04-07 15:33:21 +0800

    Remove switch in list

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,11 +1,7 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import seedu.address.model.Model;
 
@@ -16,17 +12,12 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
     public static final String MESSAGE_SUCCESS = "Listed all persons";
-    public static final Set<String> FULL_COMMAND_WORD_VARIANTS = new HashSet<>(Set.of(
-            "list", "l", "ls", "lis"
-    ));
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        if (!isNull(model.getMainWindow())) {
-            model.getMainWindow().changePanel("contacts");
-        }
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
