007bea429a951d584782371120159f43b753871f
xenyeo <yeoyiwen@gmail.com>
2025-04-08 00:02:05 +0800

    Update ug for find

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -21,7 +21,7 @@ public class StringUtil {
      *       containsWordIgnoreCase("ABc def", "AB") == false //not a full word match
      *       </pre>
      * @param sentence cannot be null
-     * @param word cannot be null, cannot be empty, must be a single word
+     * @param word cannot be null, cannot be empty
      */
     public static boolean containsWordIgnoreCase(String sentence, String word) {
         requireNonNull(sentence);
